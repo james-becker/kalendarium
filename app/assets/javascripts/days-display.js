439dee6dc30b86ee183940a75426cf48fb139341
James Becker <jamsbec@gmail.com>
2017-03-30 09:35:37 -0600

    update various

--- app/assets/javascripts/days-display.js
+++ app/assets/javascripts/days-display.js
@@ -1,5 +1,10 @@
-var counter = 0;
-setInterval(function() {
-  $(".day").eq(counter).css('opacity', '1');
-  counter = (counter + 1) % $(".day").length;
-}, 50);
+$(function() {
+  $(window).bind("load", function() {
+    var counter = 0;
+    setInterval(function() {
+      $(".day").eq(counter).css('opacity', '1');
+      counter = (counter + 1) % $(".day").length;
+    }, 50);
+  });
+})
+
